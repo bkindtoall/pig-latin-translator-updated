42b2b638accac0fd67443a3ccf7b3f5d15620b54
James Cho and Patricia Filipovic <james@>
2018-10-24 11:44:45 -0700

    last two specs, all working

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
         if (word.charAt(0)=="a" || word.charAt(0)=="e" || word.charAt(0)=="i" || word.charAt(0)=="o" || word.charAt(0)=="u"){
           result+=word + "way" + " ";
           
+        } else if (word.charAt(0)=="q" && word.charAt(1)=="u"){
+          word += word.charAt(0) + word.charAt(1) + "ay" + " ";
+          word = word.split(""); 
+          word[0] = ""; 
+          word[1] = "";
+          word = word.join("");
+          result += word;
+          console.log(word);
+
         } 
         else if (word.charAt(0)!="a" || word.charAt(0)!="e" || word.charAt(0)!="i" || word.charAt(0)!="o" || word.charAt(0)!="u"){
           word += word.charAt(0) + "ay" + " ";
@@ -16,10 +25,10 @@ $(document).ready(function(){
           word[0] = ""; 
           word = word.join("");
           result += word;
+        }
+      console.log(word);
+      console.log(result);     
 
-          console.log(word);
-          console.log(result);     
-      }
     });
 
    
