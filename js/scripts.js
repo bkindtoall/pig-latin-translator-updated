fcc22ec65375ec9217bf6b17528cec0e59c432c1
James Cho and Patricia Filipovic <james@>
2018-10-24 15:32:31 -0700

    changed to accomodate consecutive consonants meeting all objectives

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,47 @@ $(document).ready(function(){
     event.preventDefault();
     var sentence = $("input#sentence").val();
     var words = sentence.split(" ");
+    var vowels = ["a", "e", "i", "o", "u"];
+    var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
     var result="";
 
     var firstLetter = words.forEach(function(word){
-        if (word.charAt(0)=="a" || word.charAt(0)=="e" || word.charAt(0)=="i" || word.charAt(0)=="o" || word.charAt(0)=="u"){
+      if (word.charAt(0)=="q" && word.charAt(1)=="u"){
+        word += word.charAt(0) + word.charAt(1) + "ay" + " ";
+        word = word.split(""); 
+        word[0] = ""; 
+        word[1] = "";
+        word = word.join("");
+        result += word;
+
+      }   
+      else if (vowels.includes(word.charAt(0))){
           result+=word + "way" + " ";
-          
-        } else if (word.charAt(0)=="q" && word.charAt(1)=="u"){
-          word += word.charAt(0) + word.charAt(1) + "ay" + " ";
-          word = word.split(""); 
-          word[0] = ""; 
-          word[1] = "";
-          word = word.join("");
-          result += word;
-          console.log(word);
-
+          console.log("starts with vowel");
         } 
-        else if (word.charAt(0)!="a" || word.charAt(0)!="e" || word.charAt(0)!="i" || word.charAt(0)!="o" || word.charAt(0)!="u"){
-          word += word.charAt(0) + "ay" + " ";
-          word = word.split(""); 
-          word[0] = ""; 
-          word = word.join("");
-          result += word;
+      else if(consonants.includes(word.charAt(0))){
+
+        var firstLetter = word.charAt(0);
+        var count = 1;
+        
+      while(word.charAt(count) == firstLetter) {
+          count++;
         }
-      console.log(word);
-      console.log(result);     
 
-    });
+        var splice = word.slice(count) + word.slice(0, count);
+        word = splice;
+        word += "ay" + " ";
+        result += word;
 
-   
+      }
+      });
+
+  
+
     
-    firstLetter(words);
+
+    $("#output").html("<h1>"+result+"</h1>");
+
   });
 });
    
